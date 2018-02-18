d251c17e9cd7923621b45ce91edcadb147ce77e0
Steven Ventura <8stevenventura@gmail.com>
2018-02-17 18:33:41 -0600

    cardposition cancer
    reeeeeeee
    
    it kind of works. doesnt work if you drop card outside frame reeee

---
+++ UnoPlayers.lua
@@ -0,0 +1,167 @@
+--[[
+9/4/2017 Steven Ventura ("gangsthurh"), all rights reserved
+
+documentation for player object:
+
+player = {
+
+userHasUnoOrNot=false,
+userHasUnoOrNotTimer=0,
+userJoinedTheLobby=false
+
+}
+
+]]
+--[[
+
+10/23/17
+players are seen as Soccerbilly-Gorefiend
+and
+-=-=
+AddUnoPlayer("bnethashtag",butt.name,butt.presenceID,butt.glitchyAccountName);
+
+if (remainder == UNO_MESSAGE_HAS_ADDON) then
+--TODO da name match the table?
+print("|cffffff00author is " .. author);
+for i, ii in pairs(UnoPlayers) do
+print("i=" .. i)
+print("ii=" .. i
+-=-=
+all players will be identified by their Btag. i will never use their real name.
+
+
+]]
+
+--[[
+10/28/17
+the "name" attribute of player is his index in the array.
+the server host is handled weird though.
+
+On the server host's side, 
+in his array, 
+UnoHostContact.contactType = UNO_CONTACT_BTAG;
+UnoHostContact.pid = pid;
+AddUnoPlayerClientLobby("bnethashtag",author,pid);
+else
+UnoHostContact.contactType = UNO_CONTACT_WHISPER;
+UnoHostContact.whisperName = author;
+---------
+
+UnoServerPlayers DOCUMENTATION:
+[name] = {name , officialIndex};
+used for mapping player indexes to their contact informtation and other data
+]]
+
+
+
+UNO_CONTACT_WHISPER = 1;
+UNO_CONTACT_BTAG = 2;
+
+--[[this is used for screens UNO_SCREEN_SLASHUNO = 1, and UNO_SCREEN_LOBBY = 2;
+The UnoPlayers array might be used for the actual game itself ,on the server side.
+]]
+UnoPlayers = {};
+
+--[[this, UnoPlayersClientLobby, is used for the screen called UNO_SCREEN_LOBBYGUEST
+]]
+
+UnoPlayersClientLobby = {};
+
+--used by the client in the invitation phase
+function AddUnoPlayerClientLobby(boolree, nameX, pid, glitchyAccountName)
+
+----this section copied from AddUnoPlayer with 1 name change. note not all variables might be used.
+if (boolree == "bnethashtag") then
+taipu = UNO_CONTACT_BTAG;
+end
+if (boolree == "nameserver") then
+taipu = UNO_CONTACT_WHISPER;
+end
+
+UnoPlayersClientLobby[nameX] = {
+statusIcon="none",
+whisperCategory=boolree,
+userHasUnoOrNot=false,
+userHasUnoOrNotTimer=0,
+userJoinedTheLobby=false,
+userDeclinedTheInvite=false,
+contactType = taipu,
+presenceID = pid,
+isRealIDNotJustBNet = (glitchyAccountName ~= name),
+realName = glitchyAccountName,
+name = nameX
+};
+----end copy section
+
+end--end function AddUnoPlayerClientLobby
+
+--used during playing the game.
+function AddUnoPlayerClientPlaying(playerName, index)
+UnoClientPlayers[playerName] = {
+		frame = CreateFrame("FRAME",nil,UIParent),
+		officialIndex = index, name=playerName,centerX=0,centerY=0};
+UnoClientPlayers[playerName].frame:SetSize(100,100);
+UnoClientPlayers[playerName].title = 
+	UnoClientPlayers[playerName].frame:CreateFontString(nil,nil,"GameFontNormal");
+UnoClientPlayers[playerName].title:SetTextColor(1,0.643,0.169,1);
+ UnoClientPlayers[playerName].title:SetShadowColor(0,0,0,1);
+ UnoClientPlayers[playerName].title:SetShadowOffset(2,-1);
+ UnoClientPlayers[playerName].title:SetPoint("CENTER",
+	UnoClientPlayers[playerName].frame,"CENTER",0,0);
+UnoClientPlayers[playerName].title:SetText(playerName);
+UnoClientPlayers[playerName].title:Show(); 
+end--end function AddUnoPlayerClientPlaying
+
+--used by the server in the invitation phase
+function AddUnoPlayer(boolree, button)--nameX,pid,glitchyAccountName) 
+
+local taipu = "";
+if (boolree == "bnethashtag") then
+taipu = UNO_CONTACT_BTAG;
+end
+if (boolree == "nameserver") then
+taipu = UNO_CONTACT_WHISPER;
+end
+local justFirstName = "";
+if (string.find(button.toonName,"-") ~= nil) then
+justFirstName = string.sub(button.toonName,
+				1,
+				string.find(button.toonName,"-")-1);
+else
+justFirstName = button.toonName;
+end
+
+--used on the server during lobby time and transferred partially to game time
+UnoPlayers[button.name] = {
+statusIcon="none",
+whisperCategory=boolree,
+userHasUnoOrNot=false,
+userHasUnoOrNotTimer=0,
+userJoinedTheLobby=false,
+userDeclinedTheInvite=false,
+contactType = taipu,
+presenceID = button.presenceID,
+isRealIDNotJustBNet = (button.glitchyAccountName ~= button.name),
+realName = button.glitchyAccountName,
+toonName = justFirstName,
+name = button.name
+};
+
+
+
+
+end--end function AddUnoPlayer
+
+--used during lobby somewhere, and used by the server to broadcast messages during the game
+function UnoMessage(playerdata,message)
+if (playerdata.contactType == UNO_CONTACT_WHISPER) then
+
+SendChatMessage(message,"WHISPER",nil,playerdata.name);
+end
+if (playerdata.contactType == UNO_CONTACT_BTAG) then
+BNSendWhisper(playerdata.presenceID,message);
+end
+
+end--end function UnoMessage
+
+
--- UnoStates.lua
+++ UnoStates.lua
@@ -1,9 +1,26 @@
 UNO_SCREEN_BLANK = 0;
 UNO_SCREEN_SLASHUNO = 1;
 UNO_SCREEN_LOBBY = 2;
+UNO_SCREEN_INVITATION = 3;
+UNO_SCREEN_LOBBYGUEST = 4;
+UNO_SCREEN_PLAYINGGAME = 5;
 
 UnoCurrentScreen = UNO_SCREEN_BLANK;
 
+UNO_IDENTIFIER = "<Uno>";
+UNO_MESSAGE_SEND_INVITATION = "The user is trying to invite you to join his Uno group, but it appears you do not have the Uno addon (or you disabled it). pls download from curse.com/addons/wow/uno"
+UNO_MESSAGE_HAS_ADDON = "I have the uno addon";
+UNO_MESSAGE_DECLINE = "decline";
+UNO_MESSAGE_ACCEPT = "accept";
+--partial thngs
+UNO_STARTING = "starting";--DONETODO: give the name of all players appended to this string?
+UNO_MESSAGE_CARDUPDATE = "CARDUPDATE";--DONETODO: broadcast deck data appended to this string
+UNO_MESSAGE_TURNUPDATE = "TURNUPDATE";
+--[[
+UNO_TURN_UPDATE documentation:
+
+
+]]
 
 --
 UnoScreenSlashUno = CreateFrame("Frame","UnoScreenSlashUno",UIParent);
@@ -14,6 +31,14 @@ UnoScreenSlashUno.t:SetAllPoints();
 UnoScreenSlashUno.t:SetColorTexture(43/255,15/255,1/255,0.80);
 UnoScreenSlashUno:Show();
 
+CreateFrame("Button","UnoScreenSlashUnoCloseButton",UnoScreenSlashUno,"UIPanelButtonTemplate"); 
+UnoScreenSlashUnoCloseButton:SetSize(24,24);
+UnoScreenSlashUnoCloseButton:SetPoint("TOPRIGHT");
+UnoScreenSlashUnoCloseButton:SetScript("OnClick",CloseOutOfUnoGame);
+UnoScreenSlashUnoCloseButton:SetText("x");
+UnoScreenSlashUnoCloseButton:Show();
+
+
 UnoSplashFrame = CreateFrame("Frame","UnoSplashFrame",UnoScreenSlashUno);
 UnoSplashFrame:SetSize(80*2,60*2);
 UnoSplashFrame:SetPoint("TOP");
@@ -21,6 +46,9 @@ UnoSplash = UnoSplashFrame:CreateTexture();
 UnoSplash:SetTexture("Interface/AddOns/Uno/images/unologo.tga");
 UnoSplash:SetAllPoints();
 UnoSplashFrame:Show();
+
+
+
 
 CreateFrame("Frame","UnoScreenLobby",UIParent);
 UnoScreenLobby:SetSize(800,500);
@@ -30,7 +58,125 @@ UnoScreenLobby.t:SetAllPoints();
 UnoScreenLobby.t:SetColorTexture(43/255,15/255,1/255,0.80);
 UnoScreenLobby:Hide();
 
+CreateFrame("Button","UnoScreenLobbyCloseButton",UnoScreenLobby,"UIPanelButtonTemplate");
+UnoScreenLobbyCloseButton:SetSize(24,24);
+UnoScreenLobbyCloseButton:SetPoint("TOPRIGHT");
+UnoScreenLobbyCloseButton:SetScript("OnClick",CloseOutOfUnoGame);
+UnoScreenLobbyCloseButton:SetText("x");
+UnoScreenLobbyCloseButton:Show();
 
+
+UnoPlayersInLobbyList = {};
+
+function updateUnoPlayersInLobbyList()
+yOffset = 0;
+index = 0;
+for name,player in pairs(UnoPlayers) do
+index = index + 1;
+if (player.userJoinedTheLobby == true) then
+if (not(UnoPlayersInLobbyList[index])
+	or
+	UnoPlayersInLobbyList[index].name ~= name) then
+	if (UnoPlayersInLobbyList[index]) then UnoPlayersInLobbyList[index]:Hide() end
+
+text = UnoAcceptedPlayersFrame:CreateFontString(nil,nil,"GameFontNormal");
+text:SetTextColor(1,1,0,1);
+ text:SetShadowColor(0,0,0,1);
+ text:SetShadowOffset(2,-1);
+ text:SetPoint("TOPLEFT",10,-16*index);
+ text:SetText(name);
+ text:Show();
+ text.name = name;
+
+
+UnoPlayersInLobbyList[index] = text;
+end--end if not added yet or there was a change
+end--end if he is in da lobby
+end
+
+
+end--end function updateUnoPlayersInLobbyList
+
+UnoInvitationStatusList = {};
+
+function updateUnoInvitationStatusList()
+yOffset = 0;
+index = 0;
+--parallel array
+for name,player in pairs(UnoPlayers) do
+index = index + 1;
+if (not(UnoInvitationStatusList[index])
+	or
+	UnoInvitationStatusList[index].name ~= name) then
+	if (UnoInvitationStatusList[index]) then UnoInvitationStatusList[index]:Hide() end
+
+
+
+iconFrame = CreateFrame("Frame",nil,UnoProcessFrame);
+iconFrame:SetSize(16,16);
+iconFrame:SetPoint("TOPLEFT",10,-16*index);
+icon = iconFrame:CreateTexture();
+
+print("heyo guys");
+
+
+icon:SetAllPoints();
+iconFrame:Show();
+	
+text = UnoProcessFrame:CreateFontString(nil,iconFrame,"GameFontNormal");
+text:SetTextColor(1,1,0,1);
+ text:SetShadowColor(0,0,0,1);
+ text:SetShadowOffset(2,-1);
+ text:SetPoint("LEFT",iconFrame,"RIGHT",10,0);
+ text:SetText(name);
+ text:Show();
+ text.name = name;
+
+text.icon = icon;
+text.iconFrame = iconFrame;
+UnoInvitationStatusList[index] = text;
+end--end if not added yet or there was a change
+
+if (player.userHasUnoOrNot == false and player.userHasUnoOrNotTimer < 5.0) then
+UnoInvitationStatusList[index].icon:SetTexture("Interface/AddOns/Uno/images/questionmark.tga");
+
+UnoInvitationStatusList[index].iconFrame:SetScript("OnEnter", function(self)
+	GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
+	GameTooltip:SetText("checking if " .. player.name .. " has uno");
+end);
+end
+if (player.userHasUnoOrNot == false and player.userHasUnoOrNotTimer > 5.0) then
+UnoInvitationStatusList[index].icon:SetTexture("Interface/AddOns/Uno/images/redx.tga");
+UnoInvitationStatusList[index].iconFrame:SetScript("OnEnter", function(self)
+	GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
+	GameTooltip:SetText(player.name .. " does not have the addon");
+end);
+end
+if (player.userJoinedTheLobby == false and player.userHasUnoOrNot == true) then
+UnoInvitationStatusList[index].icon:SetTexture("Interface/AddOns/Uno/images/ellipsis.tga");
+UnoInvitationStatusList[index].iconFrame:SetScript("OnEnter", function(self)
+	GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
+	GameTooltip:SetText("waiting for " .. player.name .. "'s choice...");
+end);
+end
+if (player.userJoinedTheLobby == true) then
+UnoInvitationStatusList[index].icon:SetTexture("Interface/AddOns/Uno/images/greencheck.tga");
+UnoInvitationStatusList[index].iconFrame:SetScript("OnEnter", function(self)
+	GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
+	GameTooltip:SetText(player.name .. " has joined the lobby");
+end);
+end
+if (player.userDeclinedTheInvite == true) then
+UnoInvitationStatusList[index].icon:SetTexture("Interface/AddOns/Uno/images/redx.tga");
+UnoInvitationStatusList[index].iconFrame:SetScript("OnEnter", function(self)
+	GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
+	GameTooltip:SetText(player.name .. " declined your invite");
+end);
+end
+
+end--end for
+
+end--end function updateUnoInvitationStatusList
 
 CreateFrame("Button", "UnoMakeLobbyButton", UnoScreenSlashUno, "UIPanelButtonTemplate");
 UnoMakeLobbyButton:SetSize(150,50);
@@ -79,9 +225,10 @@ frame.background = texture
 local scrollframe = CreateFrame("ScrollFrame","UnoScrollFrame",UnoScrollFrameParent);
 scrollframe:SetPoint("CENTER",0,0);
 scrollframe:SetSize(150,200);
+
 scrollframe:Show();
 
-scrollbar = CreateFrame("Slider", UnoScrollBar, UnoScrollFrame, "UIPanelScrollBarTemplate") 
+scrollbar = CreateFrame("Slider", "UnoScrollBar", UnoScrollFrame, "UIPanelScrollBarTemplate") 
 scrollbar:SetPoint("TOPLEFT", frame, "TOPRIGHT", 4, -16) 
 scrollbar:SetPoint("BOTTOMLEFT", frame, "BOTTOMRIGHT", 4, 16) 
 scrollbar:SetMinMaxValues(1, 16*BNGetNumFriends()) 
@@ -104,6 +251,12 @@ end
 
 
 end) 
+
+scrollframe:EnableMouseWheel(true);
+scrollframe:SetScript("OnMouseWheel", function(self, delta)
+      scrollbar:SetValue(scrollbar:GetValue()-delta*8);
+end)
+
 local scrollbg = scrollbar:CreateTexture(nil, "BACKGROUND") 
 scrollbg:SetAllPoints(scrollbar) 
 scrollbg:SetColorTexture(0, 0, 0, 0.4) 
@@ -133,6 +286,9 @@ local button = CreateFrame("CheckButton","UnoFriendSelectButton" .. index,conten
 scrollbar.buttons[index] = button;
 button:SetPoint("TOPLEFT",_G["UnoFriendSelectButton" .. index-1],"BOTTOMLEFT",0,0);
 button.name = bnetNameWithNumber;
+button.toonName = characterName;
+button.glitchyAccountName = glitchyAccountName;
+button.presenceID = presenceID;
 button.index = index;
 button.disabled = false;
 button.setFunc = function(value) 
@@ -202,7 +358,7 @@ UnoGuildiesFrameTitle = UnoScreenLobby:CreateFontString("UnoGuildiesFrameTitle",
 UnoGuildiesFrameTitle:SetTextColor(1,1,0,1);
  UnoGuildiesFrameTitle:SetShadowColor(0,0,0,1);
  UnoGuildiesFrameTitle:SetShadowOffset(2,-1);
- UnoGuildiesFrameTitle:SetPoint("TOPLEFT",UnoScrollFrame,"BOTTOMLEFT",0,-25);
+ UnoGuildiesFrameTitle:SetPoint("TOPLEFT",UnoScrollFrame,"BOTTOMLEFT",0,-25*2);
  UnoGuildiesFrameTitle:SetText("Guildies to invite");
  UnoGuildiesFrameTitle:Show();
  
@@ -213,15 +369,17 @@ local texture2 = frame2:CreateTexture()
 texture2:SetAllPoints() 
 texture2:SetColorTexture(1,1,1,0.1) 
 frame2.background = texture2;
-local scrollframe2 = CreateFrame("ScrollFrame",nil,frame2);
+local scrollframe2 = CreateFrame("ScrollFrame","UnoScrollFrameGuildies",frame2);
 scrollframe2:SetPoint("CENTER",0,0);
 scrollframe2:SetSize(150,200);
 scrollframe2:Show();
 
-scrollbar2 = CreateFrame("Slider", nil, scrollframe2, "UIPanelScrollBarTemplate") 
+scrollbar2 = CreateFrame("Slider", "UnoGuildiesScrollBar", scrollframe2, "UIPanelScrollBarTemplate") 
 scrollbar2:SetPoint("TOPLEFT", frame2, "TOPRIGHT", 4, -16) 
 scrollbar2:SetPoint("BOTTOMLEFT", frame2, "BOTTOMRIGHT", 4, 16) 
+GuildRoster();--updates the info in the UI
 local numGuildMembers, numOnline, numOnlineAndMobile = GetNumGuildMembers()
+if (numOnline == 0) then numOnline = 1 end
 scrollbar2:SetMinMaxValues(1, 19*numOnline) 
 scrollbar2:SetValueStep(1) 
 scrollbar2.scrollStep = 1
@@ -239,11 +397,15 @@ for i, butt in ipairs(scrollbar2.buttons) do
   end
 end
 end) 
+scrollframe2:EnableMouseWheel(true);
+scrollframe2:SetScript("OnMouseWheel", function(self, delta)
+      scrollbar2:SetValue(scrollbar2:GetValue()-delta*8);
+end)
 local scrollbg2 = scrollbar:CreateTexture(nil, "BACKGROUND") 
 scrollbg2:SetAllPoints(scrollbar2) 
 scrollbg2:SetColorTexture(0, 0, 0, 0.4) 
 
-local content2 = CreateFrame("Frame", nil, scrollframe2) 
+local content2 = CreateFrame("Frame", "UnoContentFrameGuildies", scrollframe2) 
 content2:SetSize(128, 128) 
 local texture2 = content2:CreateTexture() 
 texture2:SetAllPoints() 
@@ -267,6 +429,7 @@ local fullName, rank, rankIndex, level, class, zone, note, officernote, online, 
 local button2 = CreateFrame("CheckButton","UnoGuildySelectButton" .. index,content2,"OptionsCheckButtonTemplate");
 button2:SetPoint("TOPLEFT",_G["UnoGuildySelectButton" .. index-1],"BOTTOMLEFT",0,0);
 button2.name = fullName;
+button2.toonName = fullName;
 button2.index = index;
 button2.disabled = false;
 scrollbar2.buttons[index] = button2;
@@ -340,10 +503,213 @@ UnoManualInviteEditBox:SetPoint("LEFT",UnoManualInviteEditBoxTitle)
 UnoManualInviteEditBox:SetAutoFocus(false);
 UnoManualInviteEditBox:SetScript("OnEnterPressed",function() 
 UnoManualInviteEditBox:SetText("");
-
+updateUnoInvitationStatusList();
 end);
 UnoManualInviteEditBox:Show();
 
+-----------------------
+--make da invite and refresh buttons
+CreateFrame("Button","UnoRefreshListButton",UnoScreenLobby,"UIPanelButtonTemplate");
+UnoRefreshListButton:SetSize(150,25);
+UnoRefreshListButton:SetPoint("TOP",UnoScrollFrame,"BOTTOM");
+UnoRefreshListButton:SetPoint("LEFT",UnoScrollFrame);
+UnoRefreshListButton:SetText("Refresh");
+UnoRefreshListButton:SetScript("OnClick",function()
+--refresh the bnet list. you have to reuse the old frames.
+---=-==-=begin copy paste with some changes here
+
+dummy = UnoFriendSelectButton0;
+--check boxes are 26 by 26 pixels. the gap between them is ...
+--make them all invisible now 10/27/17
+for i=1,100 do
+if (_G["UnoFriendSelectButton" .. i]) then
+_G["UnoFriendSelectButton" .. i]:Hide();
+_G["UnoFriendSelectButton" .. i]:SetChecked(false);
+_G["UnoFriendSelectButton" .. i].stevenDisable = true;
+end
+end
+
+local wowcount = 0;
+
+for index = 1, BNGetNumFriends() do
+--http://wowprogramming.com/docs/api/BNGetFriendInfo
+local presenceID,glitchyAccountName,bnetNameWithNumber,isJustBNetFriend,characterName,uselessnumber,game = BNGetFriendInfo( index );
+if (game == "WoW") then
+wowcount = wowcount + 1;
+local button = "";
+local content = UnoScrollFrameContent;
+local scrollbar = UnoScrollBar;
+local scrollframe = UnoScrollFrame;
+--reuse the old buttons on refresh
+local already = false;
+if (_G["UnoFriendSelectButton" .. index]) then
+already = true;
+button = _G["UnoFriendSelectButton" .. index];
+else
+button = CreateFrame("CheckButton","UnoFriendSelectButton" .. index,content,"OptionsCheckButtonTemplate");
+end
+button.stevenDisable = false;
+
+scrollbar.buttons[index] = button;
+button:SetPoint("TOPLEFT",_G["UnoFriendSelectButton" .. index-1],"BOTTOMLEFT",0,0);
+--write the new data
+button.name = bnetNameWithNumber;
+button.toonName = characterName;
+button.glitchyAccountName = glitchyAccountName;
+button.presenceID = presenceID;
+button.index = index;
+button.disabled = false;
+button.setFunc = function(value) 
+
+local temperino = _G["UnoFriendSelectButton" .. button.index];
+temperino.titleText:SetTextColor(1/2,0.643/2,0.169/2,1);
+temperino.disabled = true;
+end--end anonymous function
+--now make text labels
+if (already == false) then
+local titleText = button:CreateFontString("titleText",button,"GameFontNormal");
+ titleText:SetTextColor(1,0.643,0.169,1);
+ titleText:SetShadowColor(0,0,0,1);
+ titleText:SetShadowOffset(2,-1);
+ titleText:SetPoint("LEFT",button,"RIGHT",0,0);
+ titleText:SetText(bnetNameWithNumber);
+ titleText:Show();
+
+button.titleText = titleText;
+
+else--else if already
+button.titleText:SetText(bnetNameWithNumber);
+end--end if already-block
+--either way
+button:SetScript("OnEnter", function(self)
+	GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
+	GameTooltip:SetText("click to invite " .. bnetNameWithNumber);
+end);
+end--end if game==wow
+
+if (wowcount == 0)
+then scrollbar:SetMinMaxValues(1, 1);
+else
+scrollbar:SetMinMaxValues(1, 19*wowcount)  end
+end--end for
+for i, butt in ipairs(scrollbar.buttons) do
+if (butt.stevenDisable == false) then
+  if (butt:GetBottom() < UnoScrollFrame:GetBottom() or butt:GetTop() > UnoScrollFrame:GetTop()) then
+  butt:Hide();
+  else
+  butt:Show();
+  end
+end
+end
+--=-=-=-=-end copy paste
+---=-=-=begin copy paste other thing and some other copy pastes too
+--make them all invisible now 10/27/17
+for i=1,100 do
+if (_G["UnoGuildySelectButton" .. i]) then
+_G["UnoGuildySelectButton" .. i]:Hide();
+_G["UnoGuildySelectButton" .. i]:SetChecked(false);
+_G["UnoGuildySelectButton" .. i].stevenDisable = true;
+end
+end
+
+local dummy = UnoGuildySelectButton0;
+GuildRoster();--updates the info in the UI
+local numGuildMembers, numOnline, numOnlineAndMobile = GetNumGuildMembers()
+if (numOnline == 0) then numOnline = 1 end 
+GuildRoster();--updates the info in the UI
+
+for index=1,numOnline do 
+
+local fullName, rank, rankIndex, level, class, zone, note, officernote, online, status, classFileName, achievementPoints, achievementRank, isMobile, canSoR, reputation = GetGuildRosterInfo(index) 
+local already = false;
+local button2 = "";
+local content2 = UnoContentFrameGuildies;
+local scrollbar2 = UnoGuildiesScrollBar;
+local scrollframe2 = UnoScrollFrameGuildies;
+if (_G["UnoGuildySelectButton" .. index]) then
+already = true;
+button2 = _G["UnoGuildySelectButton" .. index];
+else
+button2 = CreateFrame("CheckButton","UnoGuildySelectButton" .. index,content2,"OptionsCheckButtonTemplate");
+end--end if button
+button2.stevenDisable = false;
+
+button2:SetPoint("TOPLEFT",_G["UnoGuildySelectButton" .. index-1],"BOTTOMLEFT",0,0);
+button2.name = fullName;
+button2.toonName = fullName;
+button2.index = index;
+button2.disabled = false;
+scrollbar2.buttons[index] = button2;
+button2.setFunc = function(value) 
+
+local temperino2 = _G["UnoGuildySelectButton" .. button2.index];
+temperino2.titleText2:SetTextColor(1/2,0.643/2,0.169/2,1);
+temperino2.disabled = true;
+end--end anonymous function
+--now make text labels
+if (already == false) then
+local titleText2 = button2:CreateFontString("titleText2",button2,"GameFontNormal");
+ titleText2:SetTextColor(1,0.643,0.169,1);
+ titleText2:SetShadowColor(0,0,0,1);
+ titleText2:SetShadowOffset(2,-1);
+ titleText2:SetPoint("LEFT",button2,"RIGHT",0,0);
+ titleText2:SetText(fullName);
+ titleText2:Show();
+
+button2.titleText2 = titleText2;
+
+else--else if already
+button2.titleText2:SetText(fullName);
+end
+--either way
+button2:SetScript("OnEnter", function(self)
+	GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
+	GameTooltip:SetText("click to invite " .. fullName);
+end);
+
+end--end for
+
+for i, butt in ipairs(scrollbar2.buttons) do
+if (butt.stevenDisable == false) then
+  if (butt:GetBottom() < scrollframe2:GetBottom() or butt:GetTop() > scrollframe2:GetTop()) then
+  butt:Hide();
+  else
+  butt:Show();
+  end
+end
+end
+---=-=-=end copy paste other thing
+
+
+end);--end function OnClick for refresh button
+UnoRefreshListButton:Show();
+
+CreateFrame("Button","UnoInviteListButton",UnoScreenLobby,"UIPanelButtonTemplate");
+UnoInviteListButton:SetSize(150,25);
+UnoInviteListButton:SetPoint("TOP",UnoRefreshListButton,"BOTTOM");
+UnoInviteListButton:SetPoint("LEFT",UnoScrollFrame);
+UnoInviteListButton:SetText("Invite");
+UnoInviteListButton:SetScript("OnClick",function()
+
+for i, butt in ipairs(UnoScrollBar.buttons) do
+  if (butt:GetChecked()) then
+  --AddUnoPlayer("bnethashtag",butt.name,butt.presenceID,butt.glitchyAccountName);
+  AddUnoPlayer("bnethashtag",butt);
+  UnoMessage(UnoPlayers[butt.name],UNO_IDENTIFIER .. " " .. UNO_MESSAGE_SEND_INVITATION);
+  end--end if
+end--end for
+
+for i, butt in ipairs(UnoGuildiesScrollBar.buttons) do
+  if (butt:GetChecked()) then
+  --AddUnoPlayer("nameserver",butt.name);
+  AddUnoPlayer("nameserver",butt);
+  UnoMessage(UnoPlayers[butt.name],UNO_IDENTIFIER .. " " .. UNO_MESSAGE_SEND_INVITATION);
+  end--end if
+end--end for
+
+updateUnoInvitationStatusList();
+end);
+UnoInviteListButton:Show();
 -----------------------
 --now make the ready and start buttons
 CreateFrame("Button","UnoReadyUpButton",UnoScreenLobby,"UIPanelButtonTemplate");
@@ -363,10 +729,8 @@ UnoStartGameButton:SetPoint("LEFT",UnoAcceptedPlayersFrame);
 UnoStartGameButton:SetText("Start Game");
 UnoStartGameButton:SetScript("OnClick",function()
 print("clicked da start button")
-
+StartTheUnoGameWithThesePlayers();--server command
 end);
 UnoStartGameButton:Show();
-
-
 
 end--end function UnoMakeLobbyFrames
